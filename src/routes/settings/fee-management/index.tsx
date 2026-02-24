477bb534e42206ebbe881e280b68e10799d9cf5a
himangkalal <himangkalal50@gmail.com>
2026-02-24 18:44:07 +0530

    error

--- src/routes/settings/fee-management/index.tsx
+++ src/routes/settings/fee-management/index.tsx
@@ -1,4 +1,3 @@
 import { createFileRoute } from '@tanstack/react-router';
 
-// @ts-expect-error - Route not yet generated in routeTree.gen.ts
 export const Route = createFileRoute('/settings/fee-management/')({});
